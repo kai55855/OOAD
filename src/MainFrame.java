41b88063506666107a00ee0b076702d2be56fdd7
kai <kai55855@gmail.com>
2017-11-13 16:51:57 +0800

    finish change btn color when it is being clicked

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -8,33 +8,35 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.util.Vector;
 
-public class MainFrame extends JFrame{
-    static JLabel  stateText = new JLabel("游標位置 :");
+public class MainFrame extends JFrame {
+    static JLabel stateText = new JLabel("游標位置 :");
     JMenu demo = new JMenu("demo");
     JMenu demo2 = new JMenu("demo2");
-    JMenuItem  item1 = new JMenuItem("one");
-    JMenuItem  item2 = new JMenuItem("two");
+    JMenuItem item1 = new JMenuItem("one");
+    JMenuItem item2 = new JMenuItem("two");
     JMenuBar menubar = new JMenuBar();
 
     JPanel controlPanel = new JPanel(new GridLayout(6, 1));
 
     MainCanvas mainCanvas = new MainCanvas();
-
+    Vector umlBtn = new Vector();
     SelectBtn selectBtn = new SelectBtn();
     UseCaseBtn useCaseBtn = new UseCaseBtn();
     ClassBtn classBtn = new ClassBtn();
 
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
 
 
     }
-    public MainFrame(){
+
+    public MainFrame() {
         //comit test
         //intelliji commit push test
         //branch test
         super("UML editor");
-        this.setBounds(0,0,800,800);
+        this.setBounds(0, 0, 800, 800);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.getContentPane().setLayout(new BorderLayout());
         //menu
@@ -47,9 +49,12 @@ public class MainFrame extends JFrame{
         menubar.add(demo2);
         this.setJMenuBar(menubar);
         //control plane
-        controlPanel.add(selectBtn);
-        controlPanel.add(useCaseBtn);
-        controlPanel.add(classBtn);
+        umlBtn.add(selectBtn);
+        umlBtn.add(useCaseBtn);
+        umlBtn.add(classBtn);
+        for (int i = 0; i < umlBtn.size(); i++) {
+            controlPanel.add((JButton) umlBtn.get(i));
+        }
         controlPanel.setVisible(true);
         controlPanel.setBackground(new Color(120, 166, 255));
         this.add(controlPanel, BorderLayout.WEST);
@@ -58,47 +63,59 @@ public class MainFrame extends JFrame{
         this.setVisible(true);
     }
 
-    class SelectBtn extends JButton{
-        public SelectBtn(){
+    void setBtnsToWhite() {
+        for (int i = 0; i < umlBtn.size(); i++) {
+            ((JButton)umlBtn.get(i)).setBackground(Color.white);
+        }
+    }
+
+    class SelectBtn extends JButton {
+        public SelectBtn() {
             super("Select");
             this.addActionListener(new SelectBtn.SelectBtnAction());
         }
+
         class SelectBtnAction implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent e) {
                 mainCanvas.changeMouseMode(1);
+                setBtnsToWhite();
+                setBackground(Color.gray);
             }
         }
     }
 
-    class ClassBtn extends JButton{
-        public ClassBtn(){
+    class ClassBtn extends JButton {
+        public ClassBtn() {
             super("Class");
             this.addActionListener(new ClassBtnAction());
         }
 
-        class ClassBtnAction implements ActionListener{
+        class ClassBtnAction implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent e) {
                 mainCanvas.changeMouseMode(5);
+                setBtnsToWhite();
+                setBackground(Color.gray);
             }
         }
     }
 
-    class UseCaseBtn extends JButton{
-        public UseCaseBtn(){
+    class UseCaseBtn extends JButton {
+        public UseCaseBtn() {
             super("Use Case");
             this.addActionListener(new UseCaseBtnAction());
         }
 
-        class UseCaseBtnAction implements ActionListener{
+        class UseCaseBtnAction implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent e) {
                 mainCanvas.changeMouseMode(6);
+                setBtnsToWhite();
+                setBackground(Color.gray);
             }
         }
     }
-
 
 
 }
